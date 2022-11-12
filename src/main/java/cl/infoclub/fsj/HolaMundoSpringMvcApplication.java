87f93bae48690809d91f5c44d12a7ef788b14cb9
Felipe Kessi <kessi@koders.cl>
2022-11-12 09:33:37 -0300

    primer commit Hola Mundo Spring MVC

---
+++ src/main/java/cl/infoclub/fsj/HolaMundoSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package cl.infoclub.fsj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaMundoSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaMundoSpringMvcApplication.class, args);
+	}
+
+}
