ea850eecf904e274934bfe9648638075436bb4ff
Felipe Kessi <kessi@koders.cl>
2022-11-12 09:58:09 -0300

    creacion web.xml, controller

---
+++ src/main/java/cl/infoclub/fsj/controller/HelloController.java
@@ -0,0 +1,13 @@
+package cl.infoclub.fsj.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Controller
+public class HelloController {
+
+}
