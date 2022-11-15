ce11f246fe67d1abf8214430752ee332bfe2922f
LucianoCesc <cisternas.luciano@gmail.com>
2022-11-14 21:33:41 -0300

    getter and setter

--- src/main/java/cl/infoclub/fsj/model/Mensaje.java
+++ src/main/java/cl/infoclub/fsj/model/Mensaje.java
@@ -9,6 +9,26 @@ import lombok.Setter;
 @Component("mensaje")
 public class Mensaje {
 
+	public Mensaje() {
+		super();
+	}
+	public Mensaje(String remitente, String mensaje) {
+		super();
+		this.remitente = remitente;
+		this.mensaje = mensaje;
+	}
+	public String getRemitente() {
+		return remitente;
+	}
+	public void setRemitente(String remitente) {
+		this.remitente = remitente;
+	}
+	public String getMensaje() {
+		return mensaje;
+	}
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
 	String remitente;
 	String mensaje;
 }
